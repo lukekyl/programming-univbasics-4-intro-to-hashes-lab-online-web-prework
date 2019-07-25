2095cfb6b44b765aa164f9a0023fdcb19c5cd41f
Kyle Luke <th.oldschool@gmail.com>
2019-07-25 03:19:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,11 +44,11 @@ def update_counting_hash(hash, key)
   counter = 0
   
   
-  if hash[key] == nil
-    hash[key] = 1
+  if hash[key]
+    hash[key] += 1
     
   else 
-    hash[key] += 1 
+    hash[key] = 1 
   end
   
   
